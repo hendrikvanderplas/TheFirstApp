9ba7a3b1ba787eedd2542b0907d99e315c0a63fa
Hendrik van der Plas <henkvanderplas@me.com>
2020-12-19 14:11:35 +0000

    Added generic log class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+#include "headers.h"
+
+int a = 1;
+int b = 2;
+
+void log(
+  std::string(log_string))
+{
+  std::cout << log_string << std::endl;
+};
 
 int main() {
-  std::cout << "Hello World!\n";
-}
+  
+  int result = a + b;
+  
+  std::cout << "The result: " << result << std::endl;
+  log("This is a string included into a log");
+};
